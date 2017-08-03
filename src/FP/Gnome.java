69007ac08fcdcc6ac4f6467707acffa8c1dee336
austinchowmein <30607318+austinchowmein@users.noreply.github.com>
2017-08-03 15:34:58 -0400

    synchronized some methods to avoid thread bugs

--- src/FP/Gnome.java
+++ src/FP/Gnome.java
@@ -8,87 +8,104 @@ import java.awt.Color;
 // When traveling, will be either in a village OR on a road. If the Gnome
 // is currently in a Village, onRoad will be null, and vice-versa.
 public class Gnome {
-    /* GUI */
-    public static int MAX_SIZE = 6;
-    public static int MIN_SIZE = 2;
-
-    public String toString() {
-        return this.name + "(" + this.id + ")";
-    }
-
-    /* datastructure */
+    /*GUI*/
+    public static int MAX_SIZE=6;
+    public static int MIN_SIZE=2;
+    
+    /*datastructure*/
     private int id;
     private String name;
-    private Color favColor; // @modified changed color from String to Color object
+    private Color favColor; //@modified changed color from String to Color object
     private int vipLevel; // 0 for commoners, higher values for fancier people
     private Village inVillage; // village he's in, or null if not in a village
     private Road onRoad; // Road he's on, or null if not on a road
     private MyList<Village> villageHistory; // keeps track of villages visited
+    
 
     // normal case constructor
     public Gnome(String theName, Color theFavColor, int theVIPLevel, int theID) {
-        this.id = theID;
+        this.id= theID;
         this.name = theName;
         this.favColor = theFavColor;
         this.vipLevel = theVIPLevel;
         this.onRoad = null; // not on any road yet
-        this.inVillage = null; // not in any village yet
+        this.inVillage = null;  // not in any village yet
         this.villageHistory = new MyList<Village>(); // none visited yet
     }
-
+    
     public int getID() {
         return this.id;
     }
-
+        
     public String getName() {
         return this.name;
     }
-
+    
     public void setName(String name) {
-        this.name = name;
+    	this.name = name;
     }
-
+    
     public Color getFavColor() {
         return this.favColor;
     }
-
-    public void setFavColor(Color color) {
-        this.favColor=color;
+    
+    public void setFavColor(Color c) {
+    	this.favColor = c;
     }
     
     public int getVIPLevel() {
         return this.vipLevel;
     }
-
     public void setVIPLevel(int newVIPLevel) {
         this.vipLevel = newVIPLevel;
     }
-
+    
     // return current Village. Will be null if Gnome is not in a Village now
     public Village getCurrentVillage() {
         return this.inVillage;
     }
-
-    // this is set to a village when you arrive at a village. or null
-    // if you are on a road. collect a list of all villages visited, but
-    // don't include the nulls.
-    public void setCurrentVillage(Village newVillage) {
-        this.inVillage = newVillage;
-        if (newVillage != null)
-            this.villageHistory.addIfNew(newVillage);
+    
+    public void setCurrentVillage(Village currentVillage) {
+    	this.inVillage = currentVillage;
     }
-
+        
     // return current Road if on a Road, null if not on a Road now.
     public Road getCurrentRoad() {
         return this.onRoad;
     }
-
-    public void setCurrentRoad(Road newRoad) {
-        this.onRoad = newRoad;
+    
+    // set the onRoad value to be this Road.
+    // set inVillage to be null.
+    // do nothing if passed a null argument.
+    // 
+    public synchronized void setOnRoad(Road newRoad)  {
+    	if (newRoad != null) {
+    		onRoad = newRoad;
+    		if (inVillage != null) {
+    			inVillage.removeOccupant(this);
+    			inVillage = null;
+    		}
+        }
     }
-
+    
+    // set the inVillage value to be this Village.
+    // set onRoad to be null.
+    // do nothing if passed a null argument.
+    // 
+    public synchronized void setInVillage(Village newVillage) {
+    	if (newVillage != null) {
+    		inVillage = newVillage;
+    		villageHistory.addIfNew(newVillage);
+    		if (onRoad != null) {
+    			onRoad.removeOccupant(this);
+    			onRoad = null;
+    		}
+        }
+    }
+    
     public MyList<Village> getVillageHistory() {
         return this.villageHistory;
     }
-
+    
 }
+
