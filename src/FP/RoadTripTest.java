d69cdbb3df57a64eff97b3a6c69cd46a59ef83ee
austinchowmein <30607318+austinchowmein@users.noreply.github.com>
2017-08-05 23:53:22 -0400

    wander method and other things

--- src/FP/RoadTripTest.java
+++ src/FP/RoadTripTest.java
@@ -5,18 +5,19 @@ import java.awt.Color;
 
 //class to try out our RoadTrip threading trickery on a sample Map
 public class RoadTripTest {
+	public static MapMonitor locationMonitor;
 
 	public static void main(String[] args) {
 		// simple test on simple, fixed map
-		doTest1();
-		
+		//doTest1();
+
 		// test2 generates a bigger, clustered map with the given parameters
 		// parameters for test2 are nClusters, nVillagesPerCluster, nGnomes
 		// last parameter is max capacitty for any road/village. range will
 		// be 1..max
-		doTest2(6, 5, 20, 3);
+		doTest2(6, 5, 30, 4);
 	}
-	
+
 	// simple test to set up relatively simple map and send a few
 	// Gnomes out traveling on it, printing out their progress, and
 	// some final results.
@@ -24,27 +25,27 @@ public class RoadTripTest {
 		System.out.println("Starting Test1\n");
 		Map theMap = createMap1();
 		MyList<Gnome> theGnomes = createGnomes1(theMap, 4);
-		
+
 		// send gnome 0 on a trip from node 4 to node 3, lazy mode
 		RoadTrip trip1 = new RoadTrip(theMap, theGnomes.get(0), 
 				theMap.getVillage(4), theMap.getVillage(3), RoadTrip.LAZY_MODE);
 		trip1.start();
-		
+
 		// send gnome 1 on a trip from node 2 to node 1, efficient mode
 		RoadTrip trip2 = new RoadTrip(theMap, theGnomes.get(1), 
 				theMap.getVillage(2), theMap.getVillage(1), RoadTrip.EFFICIENT_MODE);
 		trip2.start();
-		
+
 		// send gnome 2 on a trip from node 1 to node 4, lazy mode. should be a short trip
 		RoadTrip trip3 = new RoadTrip(theMap, theGnomes.get(2), 
 				theMap.getVillage(1), theMap.getVillage(4), RoadTrip.LAZY_MODE);
 		trip3.start();
-		
-		// send gnome 3 on a trip from node 4 to node 1, efficient mode
+
+		// send gnome 3 on a trip from node 4 to node 2, efficient mode
 		RoadTrip trip4 = new RoadTrip(theMap, theGnomes.get(3), 
-				theMap.getVillage(4), theMap.getVillage(1), RoadTrip.EFFICIENT_MODE);
+				theMap.getVillage(4), theMap.getVillage(2), RoadTrip.EFFICIENT_MODE);
 		trip4.start();
-		
+
 		// hang out here until all threads are done. join() waits til thread done.
 		try {
 			trip1.join();
@@ -55,7 +56,7 @@ public class RoadTripTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		/// show the gnomes' travel history
 		for (int i = 0; i < theGnomes.getSize(); i++) {
 			System.out.print(theGnomes.get(i).getName() + " has visited: ");
@@ -66,48 +67,48 @@ public class RoadTripTest {
 		}
 		System.out.println("Test1 is done.");
 	}
-	
+
 	static Map createMap1() {
 		Map m = new Map();
-		
+
 		// add some villages to the map
 		for (int i = 0; i < 6; i++) {
 			m.addVillage("Village" + i);
 		}
-		
+
 		// now add some Roads between the Villages
 		m.addRoad(0, 2, 2);
 		m.addRoad(0, 4,  4);
-		
+
 		m.addRoad(1, 3, 2);
 		m.addRoad(1, 4, 3);
-		
+
 		m.addRoad(2, 0, 2);
 		m.addRoad(2, 4, 2);
 		m.addRoad(2, 3, 1);
-		
+
 		m.addRoad(3, 0, 3);
 		m.addRoad(3, 4, 1);
-		
+
 		m.addRoad(4, 0, 4);
 		m.addRoad(4, 5, 10);
-		
+
 		m.addRoad(5, 1, 8);
 
-		
+
 		return m;
 	}
-	
+
 	static MyList<Gnome> createGnomes1(Map m, int nGnomes) {
-		
+
 		// don't worry about VIP yet...
 		for (int i = 0; i < nGnomes; i++) {
 			m.addGnome("Gnome" + i, Color.GREEN, 0);
 		}
-		
+
 		return m.getGnomes();
 	}
-	
+
 	// more complicated test with larger, clustered map.
 	// we set up a map that has the given number of clusters
 	// where each cluster has the given number of Villages in it.
@@ -119,18 +120,22 @@ public class RoadTripTest {
 	// have. min is 1.
 	static void doTest2 (int nClusters, int nVillagesPerCluster, int nGnomes,
 			int maxCapacity) {
-		
+
 		System.out.println("\n\nTest2 start with " + nClusters + " clusters of "
 				+ nVillagesPerCluster + " villages each and " + nGnomes +
 				" Gnomes...");
-		
+
 		// set up Map and Gnomes
 		Map theMap = createMap2(nClusters, nVillagesPerCluster, maxCapacity);
 		Gnome[] theGnomes = createGnomes2(theMap, nGnomes);
-		
+
 		// set up and run trips
 		RoadTrip[] trips = runTrips2(theMap, theGnomes);
-		
+
+		// run position monitor thread to update us with positions
+		locationMonitor = new MapMonitor(theMap);
+		locationMonitor.start();
+
 		// wait for all trips to end
 		for (int i = 0; i < trips.length; i++) {
 			try {
@@ -140,7 +145,7 @@ public class RoadTripTest {
 				System.out.println("Thread " + i + " was interrupted.");
 			}
 		}
-		
+
 		/// show the gnomes' travel history
 		for (int i = 0; i < theGnomes.length; i++) {
 			System.out.print(theGnomes[i].getName() + " has visited: ");
@@ -150,26 +155,26 @@ public class RoadTripTest {
 			System.out.println(".");
 		}
 		System.out.println("Test2 is done.");
-		
+
 	}
-	
+
 	// create the required number of Gnomes on the given map.
 	// use 3 VIP statuses, and 3 corresponding colors
 	static Gnome[] createGnomes2(Map m, int nGnomes) {
 		Color[] colors = {Color.RED, Color.YELLOW, Color.GREEN};
-		
+
 		Gnome[] gnomes = new Gnome[nGnomes];
 		for (int i = 0; i < nGnomes; i++) {
 			m.addGnome("Gnome"+i, colors[i%3], i%3);
 			gnomes[i] = m.getGnome(i);
 		}
-		
+
 		return gnomes;
 	}
-	
+
 	static Map createMap2(int nClusters, int nVillagesPerCluster, 
 			int maxCapacity) {
-		
+
 		// add the required number of villages to a new map. name them by
 		// cluster number and village number within that cluster, e.g., "C2V3"
 		Map m = new Map();
@@ -178,7 +183,7 @@ public class RoadTripTest {
 				m.addVillage("C"+i+"V"+j, (j%maxCapacity + 1));
 			}
 		}
-		
+
 		// now add roads. within each cluster, each village will be connected to 
 		// half of the villages in that cluster, chosen at random. what a mess.
 		Random r = new Random();
@@ -186,14 +191,14 @@ public class RoadTripTest {
 			for (int j = 0; j < nVillagesPerCluster; j++) {
 				int villageID = i * nVillagesPerCluster + j;
 				Village v = m.getVillage(villageID);
-				
+
 				// randomly choose a list of other villages in the cluster
 				// to connect to. keep choosing until we get the needed
 				// number of unique villages to connect to. we want to get
 				// nVillagesPerCluster/2 unique connections. also don't
 				// add a connection to yourself.
 				MyList<Integer> connections = new MyList<Integer>();
-				while (connections.getSize() < nVillagesPerCluster/2) {
+				while (connections.getSize() < (nVillagesPerCluster + 1)/2) {
 					int randomChoice = r.nextInt(nVillagesPerCluster) + 
 							i * nVillagesPerCluster;
 					if (randomChoice != villageID ) {
@@ -210,67 +215,78 @@ public class RoadTripTest {
 				}
 			}
 		}
-		
+
 		// when we get here, all clusters are internally connected. now add
 		// connections from the last node of each cluster to the first node of
-		// the next cluster. weight for each is cluster size.
+		// the next cluster. weight for each is based on max capacity.
 		for (int i = 0; i < nClusters; i++) {
 			// first and last clusters are special cases since they don't 
 			// have a "previous" cluster or "next" cluster, respectively
 			int thisCluster = i;
-			
+
 			int nextCluster = i + 1;
 			if (nextCluster > nClusters-1) nextCluster = 0;
-			
+
 			int prevCluster = i - 1;
 			if (prevCluster < 0) prevCluster = nClusters - 1;
-			
+
 			int firstInThisCluster = thisCluster * nVillagesPerCluster;
 			int lastInThisCluster = firstInThisCluster + nVillagesPerCluster - 1;
 			int firstInNextCluster = nextCluster * nVillagesPerCluster;
 			int lastInPrevCluster = prevCluster * nVillagesPerCluster + nVillagesPerCluster - 1;
-			
+
 			// from this cluster's last to next cluster's first
 			m.addRoad(lastInThisCluster, firstInNextCluster, nVillagesPerCluster,
-					lastInThisCluster%maxCapacity + 1);
+					(maxCapacity + 1)/2);
 			m.addRoad(firstInThisCluster, lastInPrevCluster, nVillagesPerCluster,
-					lastInPrevCluster%maxCapacity + 1);
-			
+					(maxCapacity + 1)/2);
+
 		}
 		// so now we have the right number of clusters, well connected
 		// within each cluster, but with just one 2-way connection from
 		// one cluster to the next.
 		return m;
 	}
-	
+
 	// set a bunch of Gnomes on trips.
 	// we will just pick start and destination villages at random
-	// most lazy, 1 in 4 are efficient.
+	// some lazy, some efficient, some wander.
+	// make sure number of trips to any node is smaller than
+	// that node's capacity, or things will lock up.
 	static RoadTrip[] runTrips2(Map m, Gnome[] gnomes) {
 		RoadTrip[] theTrips = new RoadTrip[gnomes.length];
 		Random r = new Random();
 
+		// use this to count up the trips going to each village
+		// so we don't overload any.
+		int[] destCount = new int[m.getVillages().getSize()];
+		for (int i = 0; i < destCount.length; i++) destCount[i]=0;
+
+		// create a trip for each Gnome
 		for (int i = 0; i < theTrips.length; i++) {
 			int startID = r.nextInt(m.getMaxVillageID() + 1);
-			int destID = r.nextInt(m.getMaxVillageID() + 1);
 
-			// dont allow start to be same as dest
-			while (destID == startID) {
+			// pick a node at random, but try again if that node
+			// already has too many trips going to it. Also
+			// try again if random picked dest == start
+			int destID = r.nextInt(m.getMaxVillageID() + 1);
+			while (destID == startID || destCount[destID] + 1 >= 
+					m.getVillages().get(destID).getCapacity()) {
 				destID = r.nextInt(m.getMaxVillageID() + 1);
 			}
+			destCount[destID]++;
 
-			// every 4th trip is EFFICIENT. otherwise LAZY.
-			if (i % 4 == 3) {
-				theTrips[i] = new RoadTrip(m, gnomes[i], m.getVillage(startID), 
-						m.getVillage(destID), RoadTrip.EFFICIENT_MODE);
-			} else {
-				theTrips[i] = new RoadTrip(m, gnomes[i], m.getVillage(startID), 
-						m.getVillage(destID), RoadTrip.LAZY_MODE);
-			}
+			// pick a mode based on mod of total mode types.
+			int mode = i%3;
+			theTrips[i] = new RoadTrip(m, gnomes[i], m.getVillage(startID), 
+					m.getVillage(destID), mode);
+
 			theTrips[i].start();
 		}
 
 		return theTrips;
 	}
 
+
 }
+
